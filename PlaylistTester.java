dd1bbea53bbde714559dee2923d3b56f178a719b
Devan Truong <dtruong28@students.polytechnic.org>
2026-01-16 10:22:51 -0800

    Added bowe's name to author in the beginning

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -3,8 +3,8 @@
  * playlist in the main method. You don't need to follow the testing specifications of this exactly
  * if you want to write your own separate tester instead.
  * Note that there's no need for a Scanner in this project-- all of the playlist can be 'hardcoded' into main.
- * @Devan
- * @01-11-2026
+ * @Devan and Bowe
+ * @01-16-2026
  */
 public class PlaylistTester {
     public static void main(String[] args) {
